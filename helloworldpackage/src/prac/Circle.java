a1d6b51af6e1229d28fa58cbb3404a3406dc724b
MediaLab <MediaLab@DESKTOP-GA3OKVH>
2023-10-06 10:44:03 +0900

    circle java

--- helloworldpackage/src/prac/Circle.java
+++ helloworldpackage/src/prac/Circle.java
@@ -8,6 +8,30 @@ public class Circle {
 	public Circle() {
 		
 	}
+	public Circle(int r,String n){
+		this.initialize(r,n);
+	}
+	
+	public void initialize(int r,String n) {
+		this.radius=r;
+		this.name=n;
+	}
+	
+	public void setRadius (int radius) {
+		this.radius =radius;
+	}
+	
+	public int getRadius() {
+		return this.radius;
+	}
+	
+	public void setName (String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
 
 	public double getPI() {
 		return this.PI;
@@ -16,7 +40,27 @@ public class Circle {
 		return PI*radius*radius;
 		
 	}
+	
+	public String toString() {
+		return "name : "+ getName()+"radius : "+getRadius();
+	}
 	public static void main(String[] args) {
 		System.out.println("Class and Objext Practice");
+		
+		Circle pizza = new Circle();
+		
+		pizza.radius=10;
+		pizza.name="Cheese Pizza";
+		
+		System.out.println(pizza.toString());
+		
+		Circle pizza2=new Circle();
+		
+		pizza2.initialize(8, "Chicago Pizza");
+		
+		System.out.println(pizza2.toString());
+		
+		Circle pizza3 =new Circle (15,"Hawaiian Pizza");
+		System.out.println(pizza3.toString());
 	}
 }
